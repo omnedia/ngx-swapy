a792e295403bbe27b64372a1effdc3e003ce9adb
Markus Block <markus.block@y-com-automation.de>
2024-09-26 16:27:39 +0200

    Changed UUID generation logic.

--- src/lib/ngx-swapy.component.ts
+++ src/lib/ngx-swapy.component.ts
@@ -1,21 +1,36 @@
-import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ContentChildren, ElementRef, EventEmitter, Input, OnDestroy, Output, QueryList, ViewChild } from '@angular/core';
-import { Config, SwapEventData, SwapEventDataData, SwapyApi, SwapyContentElement } from './ngx-swapy.types';
-import { createSwapy } from 'swapy';
-import { DomSanitizer } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
+import {
+  AfterViewInit,
+  Component,
+  ContentChildren,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+  QueryList,
+  ViewChild,
+} from "@angular/core";
+import {
+  Config,
+  SwapEventDataData,
+  SwapyContentElement,
+} from "./ngx-swapy.types";
+import { createSwapy, Swapy } from "swapy";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
-  selector: 'om-swapy',
+  selector: "om-swapy",
   standalone: true,
   imports: [CommonModule],
   templateUrl: "./ngx-swapy.component.html",
-  styleUrl: "./ngx-swapy.component.scss"
+  styleUrl: "./ngx-swapy.component.scss",
 })
 export class NgxSwapyComponent implements AfterViewInit, OnDestroy {
-  @Input('styleClass')
+  @Input("styleClass")
   styleClass?: string;
 
-  @Input('disableSwap')
+  @Input("disableSwap")
   set disableSwap(disableSwap: boolean) {
     this.swapDisabled = disableSwap;
 
@@ -24,26 +39,28 @@ export class NgxSwapyComponent implements AfterViewInit, OnDestroy {
     }
   }
 
-  @Input('swapyConfig')
+  @Input("swapyConfig")
   swapyConfig: Config = {
-    animation: 'dynamic',
+    animation: "dynamic",
     continuousMode: true,
-  }
+  };
 
   swapDisabled = false;
 
-  @Output('onSwap') onSwap = new EventEmitter<SwapEventDataData>();
+  @Output("onSwap") onSwap = new EventEmitter<SwapEventDataData>();
 
-  @ViewChild('OmSwapyContainer') containerRef!: ElementRef<HTMLElement>;
-  @ContentChildren('OmSwapyContent') swapElementRefs?: QueryList<ElementRef<HTMLElement>>;
+  @ViewChild("OmSwapyContainer") containerRef!: ElementRef<HTMLElement>;
+  @ContentChildren("OmSwapyContent") swapElementRefs?: QueryList<
+    ElementRef<HTMLElement>
+  >;
 
   swapElements: SwapyContentElement[] = [];
 
-  private swapyApi?: SwapyApi;
+  private swapyApi?: Swapy;
 
   private domChangeObserver?: MutationObserver;
 
-  constructor(private readonly sanitizer: DomSanitizer) { }
+  constructor(private readonly sanitizer: DomSanitizer) {}
 
   ngAfterViewInit(): void {
     if (!this.swapElementRefs) {
@@ -52,17 +69,18 @@ export class NgxSwapyComponent implements AfterViewInit, OnDestroy {
 
     this.swapElements = this.swapElementRefs?.toArray().map((ref) => {
       return {
-        uuids: [
-          this.generateUUID(),
-          this.generateUUID(),
-        ],
-        element: this.sanitizer.bypassSecurityTrustHtml(ref.nativeElement.outerHTML)
-      }
+        uuids: [this.generateUUID(), this.generateUUID()],
+        element: this.sanitizer.bypassSecurityTrustHtml(
+          ref.nativeElement.outerHTML,
+        ),
+      };
     });
 
     this.domChangeObserver = new MutationObserver(() => this.initSwapy());
 
-    this.domChangeObserver.observe(this.containerRef.nativeElement, { childList: true });
+    this.domChangeObserver.observe(this.containerRef.nativeElement, {
+      childList: true,
+    });
   }
 
   ngOnDestroy(): void {
@@ -77,16 +95,24 @@ export class NgxSwapyComponent implements AfterViewInit, OnDestroy {
       this.domChangeObserver = undefined;
     }
 
-    this.swapyApi = createSwapy(this.containerRef.nativeElement, this.swapyConfig);
+    this.swapyApi = createSwapy(
+      this.containerRef.nativeElement,
+      this.swapyConfig,
+    );
 
     this.swapyApi.enable(!this.swapDisabled);
 
-    this.swapyApi.onSwap((event: SwapEventData) => {
-      this.onSwap.emit(event.data);
-    })
+    this.swapyApi.onSwap((event) => {
+      const data = event.data as unknown as SwapEventDataData;
+      this.onSwap.emit(data);
+    });
   }
 
   private generateUUID(): string {
-    return crypto.randomUUID();
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (char) => {
+      const randomNum = (Math.random() * 16) | 0;
+      const value = char === "x" ? randomNum : (randomNum & 0x3) | 0x8;
+      return value.toString(16);
+    });
   }
 }
--- src/lib/ngx-swapy.types.ts
+++ src/lib/ngx-swapy.types.ts
@@ -1,39 +1,28 @@
 import { SafeHtml } from "@angular/platform-browser";
 
 export type SwapyContentElement = {
-    uuids: string[],
-    element: SafeHtml,
-}
-
-export type AnimationType = 'dynamic' | 'spring' | 'none';
-
-export type Config = {
-    animation: AnimationType;
-    continuousMode: boolean;
+  uuids: string[];
+  element: SafeHtml;
 };
 
-export type SwapCallback = (event: SwapEventData) => void;
+export type AnimationType = "dynamic" | "spring" | "none";
+
+export type Config = {
+  animation: AnimationType;
+  continuousMode: boolean;
+};
 
 export type SwapEventArray = Array<{
-    slot: string;
-    item: string | null;
+  slot: string;
+  item: string | null;
 }>;
 
-export type SwapEventData = {
-    data: SwapEventDataData;
-}
-
 export type SwapEventDataData = {
-    map: SwapEventMap;
-    array: SwapEventArray;
-    object: SwapEventObject;
-}
+  map: SwapEventMap;
+  array: SwapEventArray;
+  object: SwapEventObject;
+};
 
 export type SwapEventMap = Map<string, string | null>;
 
 export type SwapEventObject = Record<string, string | null>;
-
-export interface SwapyApi {
-    onSwap(callback: SwapCallback): void;
-    enable(enabled: boolean): void;
-}
